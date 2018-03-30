2afdc0d49cb4fa7541486f80fb3b23b634a18f46
Vinay Sharma <vinay04sharma@icloud.com>
2018-03-31 00:27:20 +0530

    fixed spinello viewer

--- DeepLearningSuite/DatasetEvaluationApp/SamplerGeneratorHandler/Viewer.cpp
+++ DeepLearningSuite/DatasetEvaluationApp/SamplerGeneratorHandler/Viewer.cpp
@@ -9,10 +9,11 @@
 #include "SamplerGenerationHandler.h"
 #include <opencv2/opencv.hpp>
 #include <glog/logging.h>
+#include <gui/Utils.h>
 
 namespace SampleGeneratorHandler {
 
-    void Viewer::process(QListView* datasetList,QListView* namesList,QListView* readerImpList,QListView* filterClasses, const std::string& datasetPath, const std::string& namesPath) {
+    void Viewer::process(QListView* datasetList,QListView* namesList,QListView* readerImpList,QListView* filterClasses, bool showDepth, const std::string& datasetPath, const std::string& namesPath) {
 
         GenericDatasetReaderPtr reader = SamplerGenerationHandler::createDatasetReaderPtr(datasetList, namesList,
                                                                                           readerImpList, filterClasses,
@@ -23,10 +24,33 @@ namespace SampleGeneratorHandler {
 
         std::string windowName="viewer";
         Sample sample;
+
+        std::vector<std::string> readerImplementation;
+        Utils::getListViewContent(readerImpList,readerImplementation,"");
+
+
         while (reader->getReader()->getNextSample(sample)){
             std::cout << "number of elements: " << sample.getRectRegions()->getRegions().size() << std::endl;
             cv::Mat image =sample.getSampledColorImage();
             cv::imshow(windowName, image);
+
+            if (showDepth) {
+                if (!sample.isDepthImageValid()) {
+                  LOG(WARNING)<< "Depth Images not available! Please verify your dataset or uncheck 'Show Depth Images'";
+                  return;
+                }
+
+                cv::Mat depth_color;
+
+                if (readerImplementation[0] == "spinello")
+                    depth_color = sample.getSampledDepthColorMapImage(-0.9345, 1013.17);
+                else
+                    depth_color = sample.getSampledDepthColorMapImage();
+
+
+                cv::imshow("Depth Color Map", depth_color);
+            }
+
             int key = cv::waitKey(0);
             if (char(key) == 'q'){
                 break;
