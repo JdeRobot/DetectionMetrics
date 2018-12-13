c3b4c8bb08d008bc5709c6ad0fc388f9ecbae37b
Preeti Mukherjee <preetimukherjee98@gmail.com>
2018-12-13 23:16:35 +0530

    Documentation added

---
+++ Documentation/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['deeplearningsuite',['DeepLearningSuite',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ Documentation/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "d",
+  1: "d"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Pages"
+};
+
