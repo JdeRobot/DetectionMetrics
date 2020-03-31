e38c868bcf5cc0783ee9a96dbdc730dd5e2fc846
Hmrishav Bandyopadhyay <hmrishavbandyopadhyay@gmail.com>
2020-03-31 10:34:05 +0530

    Fixed import_array() error

--- DeepLearningSuite/bu/detection_suite/catkin_generated/detection_suite-msg-extras.cmake.installspace.in
+++
@@ -1,2 +0,0 @@
-set(detection_suite_MESSAGE_FILES "msg/object.msg;msg/objects.msg")
-set(detection_suite_SERVICE_FILES "")
--- DeepLearningSuite/bu/devel/share/detection_suite/cmake/detection_suite-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(detection_suite_MESSAGE_FILES "/home/hmrishav/DetectionSuite/DeepLearningSuite/detection_suite/msg/object.msg;/home/hmrishav/DetectionSuite/DeepLearningSuite/detection_suite/msg/objects.msg")
-set(detection_suite_SERVICE_FILES "")
