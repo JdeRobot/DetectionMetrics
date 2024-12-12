55a759e99fbdb78a315f18dcc82a6d2f5af1c0bd
d.pascualhe <d.pascualhe@gmail.com>
2024-12-12 17:13:10 +0100

    Add Rellis3D LiDAR dataset and reduce redundancy in dataset definitions

--- detectionmetrics/datasets/dataset.py
+++ detectionmetrics/datasets/dataset.py
@@ -1,25 +1,40 @@
+from abc import ABC, abstractmethod
 import os
 import shutil
+from typing import Tuple
 from typing_extensions import Self
 
 import cv2
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
 import detectionmetrics.utils.io as uio
 
 
-class ImageSegmentationDataset:
-    """Parent image segmentation dataset class
+class SegmentationDataset(ABC):
+    """Abstract segmentation dataset class
+
+    :param dataset: Segmentation dataset as a pandas DataFrame
+    :type dataset: pd.DataFrame
+    :param dataset_dir: Dataset root directory
+    :type dataset_dir: str
+    :param ontology: Dataset ontology definition
+    :type ontology: dict
     """
 
-    def __init__(self):
-        self.dataset = pd.DataFrame([])
-        self.dataset_dir = None
-        self.ontology = {}
+    def __init__(self, dataset: pd.DataFrame, dataset_dir: str, ontology: dict):
+        self.dataset = dataset
+        self.dataset_dir = dataset_dir
+        self.ontology = ontology
 
     def __len__(self):
         return len(self.dataset)
+
+    @abstractmethod
+    def make_fname_global(self):
+        """Get all relative filenames in dataset and make global"""
+        raise NotImplementedError
 
     def append(self, new_dataset: Self):
         """Append another dataset with common ontology
@@ -38,9 +53,23 @@ class ImageSegmentationDataset:
             [self.dataset, new_dataset.dataset], verify_integrity=True
         )
 
+
+class ImageSegmentationDataset(SegmentationDataset):
+    """Parent image segmentation dataset class
+
+    :param dataset: Image segmentation dataset as a pandas DataFrame
+    :type dataset: pd.DataFrame
+    :param dataset_dir: Dataset root directory
+    :type dataset_dir: str
+    :param ontology: Dataset ontology definition
+    :type ontology: dict
+    """
+
+    def __init__(self, dataset: pd.DataFrame, dataset_dir: str, ontology: dict):
+        super().__init__(dataset, dataset_dir, ontology)
+
     def make_fname_global(self):
-        """Get all relative filenames in dataset and make global
-        """
+        """Get all relative filenames in dataset and make global"""
         if self.dataset_dir is not None:
             self.dataset["image"] = self.dataset["image"].apply(
                 lambda x: os.path.join(self.dataset_dir, x) if x is not None else None
@@ -107,3 +136,119 @@ class ImageSegmentationDataset:
 
         # Store dataset as Parquet file containing relative filenames
         self.dataset.to_parquet(os.path.join(outdir, "dataset.parquet"))
+
+
+class LiDARSegmentationDataset(SegmentationDataset):
+    """Parent lidar segmentation dataset class
+
+    :param dataset: LiDAR segmentation dataset as a pandas DataFrame
+    :type dataset: pd.DataFrame
+    :param dataset_dir: Dataset root directory
+    :type dataset_dir: str
+    :param ontology: Dataset ontology definition
+    :type ontology: dict
+    :param is_kitti_format: Whether the linked files in the dataset are stored in
+    SemanticKITTI format or not, defaults to True
+    :type is_kitti_format: bool, optional
+    """
+
+    def __init__(
+        self,
+        dataset: pd.DataFrame,
+        dataset_dir: str,
+        ontology: dict,
+        is_kitti_format: bool = True,
+    ):
+        super().__init__(dataset, dataset_dir, ontology)
+        self.is_kitti_format = is_kitti_format
+
+    def make_fname_global(self):
+        """Get all relative filenames in dataset and make global"""
+        if self.dataset_dir is not None:
+            self.dataset["points"] = self.dataset["points"].apply(
+                lambda x: os.path.join(self.dataset_dir, x) if x is not None else None
+            )
+            self.dataset["label"] = self.dataset["label"].apply(
+                lambda x: os.path.join(self.dataset_dir, x) if x is not None else None
+            )
+            self.dataset_dir = None  # dataset_dir=None -> filenames must be relative
+
+    def export(self, outdir: str):
+        """Export dataset dataframe and LiDAR files in SemanticKITTI format
+
+        :param outdir: Directory where Parquet and LiDAR files will be stored
+        :type outdir: str
+        """
+        os.makedirs(outdir, exist_ok=True)
+
+        pbar = tqdm(self.dataset.iterrows())
+
+        for sample_name, row in pbar:
+            pbar.set_description(f"Exporting sample: {sample_name}")
+
+            # Create each split directory
+            split = row["split"]
+            split_dir = os.path.join(outdir, split)
+            if not os.path.isdir(split_dir):
+                os.makedirs(split_dir, exist_ok=True)
+
+            # Init target filenames for both points and labels
+            rel_points_fname = os.path.join(split, f"points-{sample_name}.bin")
+            rel_label_fname = os.path.join(split, f"label-{sample_name}.label")
+
+            points_fname = row["points"]
+            label_fname = row["label"]
+            if self.dataset_dir is not None:
+                points_fname = os.path.join(self.dataset_dir, points_fname)
+                if label_fname:
+                    label_fname = os.path.join(self.dataset_dir, label_fname)
+
+            # If format is not appropriate: read, convert, and rewrite sample
+            if not self.is_kitti_format:
+                points = self.read_points(points_fname)
+                label, _ = self.read_label(label_fname)
+                points.tofile(os.path.join(outdir, rel_points_fname))
+                label.tofile(os.path.join(outdir, rel_label_fname))
+            else:
+                shutil.copy2(points_fname, os.path.join(outdir, rel_points_fname))
+                shutil.copy2(label_fname, os.path.join(outdir, rel_label_fname))
+
+            self.dataset.at[sample_name, "points"] = rel_points_fname
+            self.dataset.at[sample_name, "label"] = rel_label_fname
+
+        self.dataset_dir = outdir
+
+        # Write ontology and store relative path in dataset attributes
+        ontology_fname = "ontology.json"
+        self.dataset.attrs = {"ontology_fname": ontology_fname}
+        uio.write_json(os.path.join(outdir, ontology_fname), self.ontology)
+
+        # Store dataset as Parquet file containing relative filenames
+        self.dataset.to_parquet(os.path.join(outdir, "dataset.parquet"))
+
+    @staticmethod
+    def read_points(fname: str) -> np.ndarray:
+        """Read points from a binary file in SemanticKITTI format
+
+        :param fname: Binary file containing points
+        :type fname: str
+        :return: Numpy array containing points
+        :rtype: np.ndarray
+        """
+        points = np.fromfile(fname, dtype=np.float32)
+        return points.reshape((-1, 4))
+
+    @staticmethod
+    def read_label(fname: str) -> Tuple[np.ndarray, np.ndarray]:
+        """Read labels from a binary file in SemanticKITTI format
+
+        :param fname: Binary file containing labels
+        :type fname: str
+        :return: Numpy arrays containing semantic and instance labels
+        :rtype: Tuple[np.ndarray, np.ndarray]
+        """
+        label = np.fromfile(fname, dtype=np.uint32)
+        label = label.reshape((-1))
+        semantic_label = label & 0xFFFF
+        instance_label = label >> 16
+        return semantic_label.astype(np.int32), instance_label.astype(np.int32)
--- detectionmetrics/datasets/rellis3d.py
+++ detectionmetrics/datasets/rellis3d.py
@@ -1,14 +1,80 @@
 from collections import OrderedDict
 import logging
 import os
+from typing import Tuple
 
 import pandas as pd
 
-from detectionmetrics.datasets.dataset import ImageSegmentationDataset
+from detectionmetrics.datasets import dataset
 import detectionmetrics.utils.io as uio
 
 
-class Rellis3dImageSegmentationDataset(ImageSegmentationDataset):
+def build_dataset(
+    dataset_dir: str, split_fnames: dict, ontology_fname: str
+) -> Tuple[dict, dict]:
+    """Build dataset and ontology dictionaries from Rellis3D dataset structure
+
+    :param dataset_dir: Directory where both RGB images and annotations have been
+    extracted to
+    :type dataset_dir: str
+    :param split_fnames: Dictionary that contains the paths where train, val, and test
+    split files (.lst) have been extracted to
+    :type split_dir: str
+    :param ontology_fname: YAML file contained in the ontology compressed directory
+    :type ontology_fname: str
+    :return: Dataset and onotology
+    :rtype: Tuple[dict, dict]
+    """
+    # Check that provided paths exist
+    assert os.path.isdir(dataset_dir), "Dataset directory not found"
+    for split_fname in split_fnames.values():
+        assert os.path.isfile(split_fname), f"{split_fname} split file not found"
+    assert os.path.isfile(ontology_fname), "Ontology file not found"
+
+    # Load and adapt ontology
+    names, colors = uio.read_yaml(ontology_fname)
+    ontology = {names[idx]: {"idx": idx, "rgb": colors[idx]} for idx in names}
+
+    # Get samples filenames
+    train_split = [
+        s.split(" ") + ["train"] for s in uio.read_txt(split_fnames["train"])
+    ]
+    val_split = [s.split(" ") + ["val"] for s in uio.read_txt(split_fnames["val"])]
+    test_split = [s.split(" ") + ["test"] for s in uio.read_txt(split_fnames["test"])]
+
+    samples_data = train_split + val_split + test_split
+
+    # Build dataset as ordered python dictionary
+    dataset = OrderedDict()
+    skipped_samples = []
+    for data_fname, label_fname, split in samples_data:
+        sample_dir, sample_base_name = os.path.split(data_fname)
+        sample_base_name, _ = os.path.splitext(sample_base_name)
+        scene = os.path.split(os.path.split(sample_dir)[0])[-1]
+        sample_name = f"{scene}-{sample_base_name}"
+
+        data_fname = os.path.join(dataset_dir, data_fname)
+        label_fname = os.path.join(dataset_dir, label_fname)
+
+        if not os.path.isfile(data_fname) or not os.path.isfile(label_fname):
+            missing_file = "data" if not os.path.isfile(label_fname) else "label"
+            logging.warning(f"Missing {missing_file} for {sample_name}. Skipped!")
+            skipped_samples.append(sample_name)
+            continue
+
+        dataset[sample_name] = (data_fname, label_fname, scene, split)
+
+    # Report results
+    print(f"Samples retrieved: {len(dataset)} / {len(samples_data)}")
+    if skipped_samples:
+        print("Skipped samples:")
+        for sample_name in skipped_samples:
+            print(f"\n\t{sample_name}")
+
+    return dataset, ontology
+
+
+class Rellis3dImageSegmentationDataset(dataset.ImageSegmentationDataset):
     """Specific class for Rellis3D-styled image segmentation datasets. All data can
     be downloaded from the official repo (https://github.com/unmannedlab/RELLIS-3D):
         images   -> https://drive.google.com/file/d/1F3Leu0H_m6aPVpZITragfreO_SGtL2yV
@@ -27,56 +93,47 @@ class Rellis3dImageSegmentationDataset(ImageSegmentationDataset):
     """
 
     def __init__(self, dataset_dir: str, split_dir: str, ontology_fname: str):
-        super().__init__()
-
-        # Check that provided paths exist
-        assert os.path.isdir(dataset_dir), "Dataset directory not found"
-        assert os.path.isdir(split_dir), "Split directory not found"
-        assert os.path.isfile(ontology_fname), "Ontology file not found"
-
-        # Load and adapt ontology
-        names, colors = uio.read_yaml(ontology_fname)
-        self.ontology = {names[idx]: {"idx": idx, "rgb": colors[idx]} for idx in names}
-
-        # Get samples filenames
-        train_split = uio.read_txt(os.path.join(split_dir, "train.lst"))
-        val_split = uio.read_txt(os.path.join(split_dir, "val.lst"))
-        test_split = uio.read_txt(os.path.join(split_dir, "test.lst"))
-
-        train_split = [s.split(" ") + ["train"] for s in train_split]
-        val_split = [s.split(" ") + ["val"] for s in val_split]
-        test_split = [s.split(" ") + ["test"] for s in test_split]
-
-        samples_data = train_split + val_split + test_split
-
-        # Build dataset as ordered python dictionary
-        dataset = OrderedDict()
-        skipped_samples = []
-        for image_fname, label_fname, split in samples_data:
-            sample_dir, sample_base_name = os.path.split(image_fname)
-            sample_base_name, _ = os.path.splitext(sample_base_name)
-            scene = os.path.split(os.path.split(sample_dir)[0])[-1]
-            sample_name = f"{scene}-{sample_base_name}"
-
-            image_fname = os.path.join(dataset_dir, image_fname)
-            label_fname = os.path.join(dataset_dir, label_fname)
-
-            if not os.path.isfile(image_fname) or not os.path.isfile(label_fname):
-                missing_file = "image" if not os.path.isfile(label_fname) else "label"
-                logging.warning(f"Missing {missing_file} for {sample_name}. Skipped!")
-                skipped_samples.append(sample_name)
-                continue
-
-            dataset[sample_name] = (image_fname, label_fname, scene, split)
+        split_fnames = {
+            "train": os.path.join(split_dir, "train.lst"),
+            "val": os.path.join(split_dir, "val.lst"),
+            "test": os.path.join(split_dir, "test.lst"),
+        }
+        dataset, ontology = build_dataset(dataset_dir, split_fnames, ontology_fname)
+        super().__init__(dataset, dataset_dir, ontology)
 
         # Convert to Pandas
-        cols = ["image", "label", "scene", "split"]
+        cols = ["images", "label", "scene", "split"]
         self.dataset = pd.DataFrame.from_dict(dataset, orient="index", columns=cols)
         self.dataset.attrs = {"ontology": self.ontology}
 
-        # Report results
-        print(f"Samples retrieved: {len(dataset)} / {len(samples_data)}")
-        if skipped_samples:
-            print("Skipped samples:")
-            for sample_name in skipped_samples:
-                print(f"\n\t{sample_name}")
+
+class Rellis3dLiDARSegmentationDataset(dataset.LiDARSegmentationDataset):
+    """Specific class for Rellis3D-styled LiDAR segmentation datasets. All data can
+    be downloaded from the official repo (https://github.com/unmannedlab/RELLIS-3D):
+        points   -> https://drive.google.com/file/d/1lDSVRf_kZrD0zHHMsKJ0V1GN9QATR4wH
+        labels   -> https://drive.google.com/file/d/12bsblHXtob60KrjV7lGXUQTdC5PhV8Er
+        split    -> https://drive.google.com/file/d/1raQJPySyqDaHpc53KPnJVl3Bln6HlcVS
+        ontology -> https://drive.google.com/file/d/1K8Zf0ju_xI5lnx3NTDLJpVTs59wmGPI6
+
+    :param dataset_dir: Directory where both points and labels have been extracted to
+    :type dataset_dir: str
+    :param split_dir: Directory where train, val, and test files (.lst) have been
+    extracted to
+    :type split_dir: str
+    :param ontology_fname: YAML file contained in the ontology compressed directory
+    :type ontology_fname: str
+    """
+
+    def __init__(self, dataset_dir: str, split_dir: str, ontology_fname: str):
+        split_fnames = {
+            "train": os.path.join(split_dir, "pt_train.lst"),
+            "val": os.path.join(split_dir, "pt_val.lst"),
+            "test": os.path.join(split_dir, "pt_test.lst"),
+        }
+        dataset, ontology = build_dataset(dataset_dir, split_fnames, ontology_fname)
+        super().__init__(dataset, dataset_dir, ontology)
+
+        # Convert to Pandas
+        cols = ["points", "label", "scene", "split"]
+        self.dataset = pd.DataFrame.from_dict(dataset, orient="index", columns=cols)
+        self.dataset.attrs = {"ontology": self.ontology}
